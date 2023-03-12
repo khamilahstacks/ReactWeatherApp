753be56fd3e10a3d0f5cc96784418a7e7509541b
khamilahstacks <kayanna87829@gmail.com>
2023-03-12 04:38:15 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">  Weather App
+    <div className="container">
     <footer>
       This Project was coded by Khamilah Prevost and is {""} <a href="https://github.com/khamilahstacks/ReactWeatherApp" target="_blank"> open-sourced on Github</a>
     </footer>
+    </div>
     </div>
   );
 }
