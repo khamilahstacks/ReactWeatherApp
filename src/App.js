fecba40313a8190ed55db3a8e4cdb323d65a9fce
khamilahstacks <kayanna87829@gmail.com>
2023-03-12 11:07:13 -0700

    added weather css js and forcast components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React from "react";
-import './App.css';
-export default function App()
-function App() {
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
   return (
-    <div className="App">  Weather App
-    <div className="container">
-    <footer>
-      This Project was coded by Khamilah Prevost and is {""} <a href="https://github.com/khamilahstacks/ReactWeatherApp" target="_blank"> open-sourced on Github</a>
-    </footer>
-    </div>
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Los Angeles" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/khamilahstacks/ReactWeatherApp.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Khamilah Prevost
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/khamilahstacks/ReactWeatherApp.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
+
---
+++ src/Weatherforcast.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./WeatherForecast.css";
+import Axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = `bc5ca568ee2d7c71357ca430a3ff8705`;
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    Axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
+}
