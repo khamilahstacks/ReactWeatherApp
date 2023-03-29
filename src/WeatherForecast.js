d7be7ea24adc541c1f5a62fbab48ae6751f30cc9
khamilahstacks <kayanna87829@gmail.com>
2023-03-28 22:56:28 -0700

    made changes to the api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -20,11 +20,11 @@ export default function WeatherForecast(props) {
     let apiKey = `bc5ca568ee2d7c71357ca430a3ff8705`;
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
   }
-
+  
   if (loaded) {
     return (
       <div className="WeatherForecast">
