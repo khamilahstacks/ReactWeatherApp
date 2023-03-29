93383b6d8c5ec7b8969f894e302f1ad005d150b3
khamilahstacks <kayanna87829@gmail.com>
2023-03-28 22:18:36 -0700

    adjusted format and background

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -15,8 +15,9 @@ export default function WeatherForecastDay(props) {
   function day() {
     let date = new Date(props.data.dt * 1000);
     let day = date.getDay;
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    return day();
+    return days[day];
   }
 
   return (
